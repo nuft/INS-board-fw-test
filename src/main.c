843dbcd5a8381b54f12aaa3002cf4e2f76c0f89d
Michael Spieler <the.chaotic.coder@gmail.com>
2014-08-03 14:12:34 +0200

    Added LED blinky code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+void delay(unsigned int n)
+{
+    while (n-- > 0) {
+        __asm__ volatile ("nop":::);
+    }
+}
+
+
 int main(void)
 {
-    while (1);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO10);
+
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO14);
+
+    while (1) {
+        gpio_toggle(GPIOA, GPIO8 | GPIO10);
+        gpio_toggle(GPIOB, GPIO14);
+        delay(1000000);
+    }
     return 0;
 }
